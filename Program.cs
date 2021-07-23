27549d8a5eeb1b889d943b3a9e6938b56bbe8227
fsalinas <fsalinas@outlook.com>
2021-07-23 08:09:11 -0500

    ejemplo de carga de datos

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.IO;
 using dev_m2.task._03.Models;
 
 namespace dev_m2.task._03
@@ -10,7 +11,98 @@ namespace dev_m2.task._03
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Mi primer acceso a datos.");
+            string path = System.IO.Path.GetFullPath("Archivos/EntidadFederativa.csv");
+/*
+           var datos = File.ReadAllLines(path);
+            foreach(var ln in datos){
+                if(ln.Length>1){
+                    // Console.WriteLine($"{contador ++}   ---   {linea}");
+                    var dato = ln.Split(',');
+                    var id = dato[0].Replace('"',' ');
+                    var nombre  = dato[1];
+                    if(nombre.Length > 1){
+                        var nombreModificado = nombre.Substring(1,nombre.Length-2);
+                            Console.WriteLine($"{nombreModificado}");  
+                            nombre = nombreModificado;  
+                    }
 
+
+                    var abrev  = dato[2].Replace('"',' ');
+                    var poblacionTotal = dato[3].Replace('"',' ');
+                    var poblacionMasculina = dato[5].Replace('"',' ');
+                    var poblacionFemenina = dato[6].Replace('"',' ');
+                    
+                   
+                    int entidadID = 0;
+                    
+                    if( int.TryParse(id,out entidadID)) {
+                        using(var conexion = new ElcoBitContext()  ){
+
+                            EntidaFederativa nuevaEntidad = new EntidaFederativa();
+                            nuevaEntidad.EntidadId = entidadID;
+                            nuevaEntidad.Nombre = nombre;
+                            nuevaEntidad.NombreAbreviado = abrev;
+                            
+                            nuevaEntidad.PoblacionFemenina = Convert.ToInt32(poblacionFemenina);
+                            nuevaEntidad.PoblacionMasculina = Convert.ToInt32(poblacionMasculina);
+                            nuevaEntidad.PoblacionTotal = Convert.ToInt32(poblacionTotal);
+
+                            conexion.EntidaFederativas.Add(nuevaEntidad);
+                            conexion.SaveChanges();
+                        }
+                    }
+                }
+            }*/
+
+            using (StreamReader archivo = File.OpenText(path))
+            {
+                string linea = "";
+
+                          
+                while ((linea = archivo.ReadLine()) != null)
+                {
+
+                    if(linea.Length>1){
+                    // Console.WriteLine($"{contador ++}   ---   {linea}");
+                    var dato = linea.Split(',');
+                    var id = dato[0].Replace('"',' ');
+                    var nombre  = dato[1];
+                    if(nombre.Length > 1){
+                        var nombreModificado = nombre.Substring(1,nombre.Length-2);
+                            Console.WriteLine($"{nombreModificado}");  
+                            nombre = nombreModificado;  
+                    }
+
+
+                    var abrev  = dato[2].Replace('"',' ');
+                    var poblacionTotal = dato[3].Replace('"',' ');
+                    var poblacionMasculina = dato[5].Replace('"',' ');
+                    var poblacionFemenina = dato[6].Replace('"',' ');
+                  
+                    int entidadID = 0;
+                    
+                    if( int.TryParse(id,out entidadID)) {
+                        using(var conexion = new ElcoBitContext()  ){
+
+                            EntidaFederativa nuevaEntidad = new EntidaFederativa();
+                            nuevaEntidad.EntidadId = entidadID;
+                            nuevaEntidad.Nombre = nombre;
+                            nuevaEntidad.NombreAbreviado = abrev;
+                            
+                            nuevaEntidad.PoblacionFemenina = Convert.ToInt32(poblacionFemenina);
+                            nuevaEntidad.PoblacionMasculina = Convert.ToInt32(poblacionMasculina);
+                            nuevaEntidad.PoblacionTotal = Convert.ToInt32(poblacionTotal);
+
+                            conexion.EntidaFederativas.Add(nuevaEntidad);
+                            conexion.SaveChanges();
+                        }
+                    }
+                }
+                }
+            }
+
+
+/*
             using(var conexion = new ElcoBitContext()  ){
 
                     EntidaFederativa nuevaEntidad = new EntidaFederativa();
@@ -39,7 +131,7 @@ namespace dev_m2.task._03
                 } 
 
 
-            }
+            }*/
 
              
 
